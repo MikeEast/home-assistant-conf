02f97b633a07ee1898c677b5af468d7eb88147bf
MikeEast <mikael@inloop.se>
2017-12-11 23:41:09 +0100

    Ios and startup things

---
+++ python_scripts/device_tracker_placeholders.py
@@ -0,0 +1,14 @@
+hass.states.set('device_tracker.micke', 'unknown', {
+    'name': 'Micke',
+    'last_update_source': 'placeholder'
+})
+
+hass.states.set('device_tracker.hanna', 'unknown', {
+    'name': 'Hanna',
+    'last_update_source': 'placeholder'
+})
+
+hass.states.set('device_tracker.filippa', 'unknown', {
+    'name': 'Filippa',
+    'last_update_source': 'placeholder'
+})
